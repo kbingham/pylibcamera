7701e5119a3348c53c29b64af191d066da91dfad
meawoppl <meawoppl@gmail.com>
2022-02-25 22:56:01 -0800

    WIP

--- pylibcamera/wrapper_test.py
+++ pylibcamera/wrapper_test.py
@@ -6,9 +6,10 @@ import pylibcamera.wrapper
 
 
 class TestLibCameraWrapper(unittest.TestCase):
-    # def test_camera_manager_wrapper(self):
-    #     camera_manager = pylibcamera.wrapper.PyCameraManager()
-    #     assert camera_manager.get_n_cameras() >= 0
+    def test_camera_manager_wrapper(self):
+        camera_manager = pylibcamera.wrapper.PyCameraManager()
+        assert camera_manager.get_n_cameras() == 0
 
-    def test_get_camera(self):
+    def test_everything(self):
+        self.skipTest("Requires a media device")
         camera = pylibcamera.wrapper.LibCameraWrapper(0)
