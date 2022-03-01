bb1450dc6465c89e5561ee36c2504789b1b8c824
Kieran Bingham <kieran.bingham@ideasonboard.com>
2022-03-01 23:03:10 +0000

    setup.py: Use pkg-config to identify libcamera
    libcamera can be installed by a distribution or from source and
    the paths to include should be obtained by the pkg-config tool
    
    Provide a helper to obtain those arguments, and use them when
    creating the pylibcamera Extension.
    
    This brings in camera_base as an additional library linkage
    which was previously missed, and ensures consistent links
    to the required include dirs.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- setup.py
+++ setup.py
@@ -2,12 +2,27 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+import subprocess
+
+# https://stackoverflow.com/questions/60174152/how-do-i-add-pkg-config-the-setup-py-of-a-cython-wrapper
+def pkgconfig(package):
+    # initialise kw to support pkg-config empty args
+    kw = { 'include_dirs': [], 'library_dirs': [], 'libraries': [], }
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    output = subprocess.getoutput(
+        'pkg-config --cflags --libs {}'.format(package))
+    for token in output.strip().split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+
+libcamera_args = pkgconfig('libcamera')
+
 ext = Extension(
     'pylibcamera.wrapper', 
     sources=["pylibcamera/wrapper.pyx"],
-    include_dirs = ["/usr/local/include/libcamera"],
-    library_dirs = ["/usr/local/lib/x86_64-linux-gnu"],
-    libraries=["camera"],
+    include_dirs=libcamera_args["include_dirs"],
+    library_dirs = libcamera_args['library_dirs'],
+    libraries = libcamera_args['libraries'],
     extra_compile_args= ["-std=c++17"],
     language="c++")
 
